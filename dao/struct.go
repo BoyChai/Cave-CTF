b13cf1fa85281d2c5675d9a1a2689c883b05c5eb
BoyChai <1972567225@qq.com>
2023-10-26 18:49:50 +0800

    Update User Table Structure

--- dao/struct.go
+++ dao/struct.go
@@ -11,7 +11,7 @@ var db *gorm.DB
 
 // QuestionList 题目表
 type QuestionList struct {
-	ID       uint   `gorm:"primaryKey;auto_increment" form:"name"`
+	ID       uint   `gorm:"primaryKey;auto_increment" form:"id"`
 	Title    string `form:"title"`
 	Describe string `form:"describe"`
 	Flag     string `form:"flag"`
@@ -22,14 +22,15 @@ type QuestionList struct {
 
 // Users 用户表
 type Users struct {
-	ID    uint   `gorm:"primaryKey;auto_increment" form:"name"`
+	ID    uint   `gorm:"primaryKey;auto_increment" form:"id"`
 	Name  string `form:"name"`
+	Alias string `form:"alias"`
 	Score string `form:"score"`
 }
 
 // Ranking 成绩表
 type Ranking struct {
-	ID            uint   `json:"ID,omitempty;auto_increment" form:"name"`
+	ID            uint   `json:"ID,omitempty;auto_increment" form:"id"`
 	User          string `form:"user"`
 	Score         string `form:"score"`
 	QuestionTitle string `form:"question"`

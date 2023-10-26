7332560cc0cb37cebcfca040f8b473bfa4b33711
BoyChai <1972567225@qq.com>
2023-10-26 18:59:29 +0800

    Update Router Request Parameters

--- dao/struct.go
+++ dao/struct.go
@@ -12,26 +12,26 @@ var db *gorm.DB
 // QuestionList 题目表
 type QuestionList struct {
 	ID       uint   `gorm:"primaryKey;auto_increment" form:"id"`
-	Title    string `form:"title"`
-	Describe string `form:"describe"`
-	Flag     string `form:"flag"`
-	Type     string `form:"type"`
-	Annex    string `form:"annex"`
-	Score    string `form:"score"`
+	Title    string `form:"title" binding:"required"`
+	Describe string `form:"describe" binding:"required"`
+	Flag     string `form:"flag" binding:"required"`
+	Type     string `form:"type" binding:"required"`
+	Annex    string `form:"annex" binding:"required"`
+	Score    string `form:"score" binding:"required"`
 }
 
 // Users 用户表
 type Users struct {
 	ID    uint   `gorm:"primaryKey;auto_increment" form:"id"`
-	Name  string `form:"name"`
-	Alias string `form:"alias"`
+	Name  string `form:"name" binding:"required"`
+	Alias string `form:"alias" binding:"required"`
 	Score string `form:"score"`
 }
 
 // Ranking 成绩表
 type Ranking struct {
 	ID            uint   `json:"ID,omitempty;auto_increment" form:"id"`
-	User          string `form:"user"`
-	Score         string `form:"score"`
-	QuestionTitle string `form:"question"`
+	User          string `form:"user" binding:"required"`
+	Score         string `form:"score" binding:"required"`
+	QuestionTitle string `form:"question" binding:"required"`
 }
